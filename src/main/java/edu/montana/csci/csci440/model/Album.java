e21319d8ccf40850505462b8f9bc020c02325357
ahmed <a.bennaji96@gmail.com>
2020-10-19 14:37:07 -0600

    passed paging tests

--- src/main/java/edu/montana/csci/csci440/model/Album.java
+++ src/main/java/edu/montana/csci/csci440/model/Album.java
@@ -64,9 +64,12 @@ public class Album extends Model {
     public static List<Album> all(int page, int count) {
         try (Connection conn = DB.connect();
              PreparedStatement stmt = conn.prepareStatement(
-                     "SELECT * FROM albums LIMIT ?"
+                     "SELECT * FROM albums LIMIT ? OFFSET ?"
+
              )) {
-            stmt.setInt(1, count);
+
+            stmt.setInt(1, count );
+            stmt.setInt(2,(page-1)*count);
             ResultSet results = stmt.executeQuery();
             List<Album> resultList = new LinkedList<>();
             while (results.next()) {
@@ -92,9 +95,57 @@ public class Album extends Model {
             throw new RuntimeException(sqlException);
         }
     }
+    @Override
+    public boolean create() {
+        if (verify()) {
+            try (Connection conn = DB.connect();
+                 PreparedStatement stmt = conn.prepareStatement(
+                         "INSERT INTO albums (title) VALUES (?)")) {
+                stmt.setString(1, this.getTitle());
+
+                stmt.executeUpdate();
+                albumId= DB.getLastID(conn);
+                return true;
+            } catch (SQLException sqlException) {
+                throw new RuntimeException(sqlException);
+            }
+        } else {
+            return false;
+        }
+    }
+    @Override
+    public boolean verify() {
+        _errors.clear(); // clear any existing errors
+        if (title == null || "".equals(title)) {
+            addError("album title can't be null or blank!");
+        }
+        if( artistId == null ||"".equals((albumId))){
+            addError("artist Id cannot be null ");
+        }
+
+        return !hasErrors();
+    }
+    @Override
+    public boolean update() {
+        if (verify()) {
+            try (Connection conn = DB.connect();
+                 PreparedStatement stmt = conn.prepareStatement(
+                         "UPDATE albums SET title =?")) {
+                stmt.setString(1, this.getTitle());
+                stmt.executeUpdate();
+                return true;
+            } catch (SQLException sqlException) {
+                throw new RuntimeException(sqlException);
+            }
+        } else {
+            return false;
+        }
+    }
+
 
     public static List<Album> getForArtist(Long artistId) {
         // TODO implement
+
         return Collections.emptyList();
     }
 
--- src/main/java/edu/montana/csci/csci440/model/Artist.java
+++ src/main/java/edu/montana/csci/csci440/model/Artist.java
@@ -51,9 +51,11 @@ public class Artist extends Model {
     public static List<Artist> all(int page, int count) {
         try (Connection conn = DB.connect();
              PreparedStatement stmt = conn.prepareStatement(
-                     "SELECT * FROM artists LIMIT ?"
+                     "SELECT * FROM artists LIMIT ? OFFSET ?  "
              )) {
-            stmt.setInt(1, count);
+            stmt.setInt(1, count );
+            stmt.setInt(2,(page-1)*count);
+
             ResultSet results = stmt.executeQuery();
             List<Artist> resultList = new LinkedList<>();
             while (results.next()) {
@@ -79,5 +81,51 @@ public class Artist extends Model {
             throw new RuntimeException(sqlException);
         }
     }
+
+    @Override
+    public boolean create() {
+        if (verify()) {
+            try (Connection conn = DB.connect();
+                 PreparedStatement stmt = conn.prepareStatement(
+                         "INSERT INTO artists (name) VALUES (?)")) {
+                stmt.setString(1, this.getName());
+
+                stmt.executeUpdate();
+                artistId = DB.getLastID(conn);
+                return true;
+            } catch (SQLException sqlException) {
+                throw new RuntimeException(sqlException);
+            }
+        } else {
+            return false;
+        }
+    }
+    @Override
+    public boolean verify() {
+        _errors.clear(); // clear any existing errors
+        if (name == null || "".equals(name)) {
+            addError("artist name can't be null or blank!");
+        }
+
+        return !hasErrors();
+    }
+    @Override
+    public boolean update() {
+        if (verify()) {
+            try (Connection conn = DB.connect();
+                 PreparedStatement stmt = conn.prepareStatement(
+                         "UPDATE artists SET name =?")) {
+                stmt.setString(1, this.getName());
+                stmt.executeUpdate();
+                return true;
+            } catch (SQLException sqlException) {
+                throw new RuntimeException(sqlException);
+            }
+        } else {
+            return false;
+        }
+    }
+
+
 
 }
